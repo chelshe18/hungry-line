5ca73b72c6195c759a6210d2f61433eb9ba90bc7
Chelsea He-Chen <chehechen@gmail.com>
2023-07-30 16:46:40 -0500

    set up native stack navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Notification from "../screens/notification";
 import Hungryline from "../screens/hungryline";
+import Login from "../screens/login";
+
+export type RootStackParamList = {
+  Hungryline: undefined;
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 export default function App() {
   return (
-    <>
-      <StatusBar style="auto" />
-      
-    </>
+    // <Hungryline />
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Hungryline"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Hungryline" component={Hungryline} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
