11ad2c9f15176c690899b78046e1bc1edc2d3307
Chelsea He-Chen <chehechen@gmail.com>
2023-08-04 12:19:49 -0500

    removed unused StyleSheet

--- src/App.tsx
+++ src/App.tsx
@@ -84,11 +84,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
