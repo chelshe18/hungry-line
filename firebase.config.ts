7667eec23b47a53382463280ceb91574e10f894d
ANHTRAN <tran36a@mtholyoke.edu>
2023-08-06 18:31:14 +0700

    finish the join queue

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore"; 
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,3 +17,5 @@ const firebaseConfig = {
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const storage = getStorage(FIREBASE_APP);
+export const db = getFirestore(FIREBASE_APP);
