092de570aeedbcb8685bb9f31629d1af2ec4be0c
Chelsea He-Chen <chehechen@gmail.com>
2023-08-08 16:32:07 -0500

    loaded dining halls from firestore

--- src/screens/dining.tsx
+++ src/screens/dining.tsx
@@ -1,85 +1,157 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Modal,
+  TextInput,
+} from "react-native";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../App";
+import { FIREBASE_AUTH, db } from "../../firebase.config";
+import { Ionicons } from "@expo/vector-icons";
 import Yellow from "../components/yellow_ellipse";
 import Filter from "../components/filter_button";
 import { SearchBar } from "react-native-screens";
 import FilterComponent from "../components/filter_compon";
+import Card from "../components/card";
 import Button from "../components/button";
 
 type DiningProps = NativeStackScreenProps<RootStackParamList, "Dining">;
 
 export default function Dining({ navigation }: DiningProps) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.top}>
-                <Yellow />
-                <Image style={styles.profilepic} source={require("../assets/profile.png")} />
-                <Text style={styles.welcomeText}>Welcome [Name]</Text>
-            </View>
-            <View style={styles.bottom}>
-                <View style={styles.searchBarContainer}>
-                    {/* <SearchBar
+  const auth = FIREBASE_AUTH;
+  const [modalOpen, setModalOpen] = useState(false);
+  const [idArray, setIdArray] = useState<string[]>([]);
+  const images = new Map([
+    ["community-dining-center", require("../assets/dining_hall_1.jpg")],
+    ["downtown-hall", require("../assets/dining_hall_2.jpg")],
+    ["dumpling-dining-center", require("../assets/dining_hall_3.jpg")],
+  ]);
+  const ids: string[] = [];
+
+  const q = query(collection(db, "dining-halls"));
+  getDocs(q)
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        ids.push(doc.id);
+      });
+    })
+    .then(() => {
+      setIdArray(ids);
+    });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Yellow />
+        <Image
+          style={styles.profilepic}
+          source={require("../assets/profile.png")}
+        />
+        <Text style={styles.welcomeText}>
+          Welcome {auth.currentUser?.displayName}
+        </Text>
+      </View>
+      <View style={styles.bottom}>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.searchBarContainer}>
+            {/* <SearchBar
                         placeholder="Search Dining and Menus"
                         onChangeText={this.updateSearch}
                         value={search}> */}
-                    <Image source={require("../assets/searchpic.png")} />
+            <Image
+              source={require("../assets/searchpic.png")}
+              style={{ height: 35, width: 35 }}
+            />
 
-                    {/* </SearchBar> */}
-                </View>
-                <Filter />
-                {/* <FilterComponent /> */}
-            </View>
-        </View >
-    )
+            {/* </SearchBar> */}
+          </View>
+          <Ionicons
+            name="filter"
+            size={24}
+            color="black"
+            onPress={() => {
+              setModalOpen(true);
+            }}
+            style={{ paddingLeft: 10 }}
+          />
+        </View>
+
+        <ScrollView>
+          {idArray.map((id) => (
+            <Card
+              key={id}
+              id={id}
+              image={images.get(id)}
+              onPress={() => navigation.navigate("QueueStatus")}
+            />
+          ))}
+        </ScrollView>
+
+        {/* <Filter /> */}
+        <Modal visible={modalOpen} animationType="slide" transparent={true}>
+          <Ionicons
+            name="close"
+            size={24}
+            onPress={() => {
+              setModalOpen(false);
+            }}
+          />
+          <FilterComponent />
+        </Modal>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        display: "flex",
-        flexDirection: "column",
-    },
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+  },
 
-    top: {
-        backgroundColor: "#A4D0A4",
-        height: "40%",
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-    },
+  top: {
+    backgroundColor: "#A4D0A4",
+    height: "40%",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+  },
 
-    profilepic: {
-        width: 130,
-        height: 129,
-        display: "flex",
-        marginTop: 60,
-    },
+  profilepic: {
+    width: 130,
+    height: 129,
+    display: "flex",
+    marginTop: 60,
+  },
 
-    welcomeText: {
-        color: "#617A55",
-        // fontfamily: "Poppins",
-        fontSize: 24,
-        fontStyle: "normal",
-        fontWeight: "700",
-        lineHeight: 23,
-        marginTop: 25,
-    },
+  welcomeText: {
+    color: "#617A55",
+    fontFamily: "poppins-semibold",
+    fontSize: 20,
+    lineHeight: 23,
+    marginTop: 25,
+  },
 
-    bottom: {
-        backgroundColor: "#FFF8D6",
-        height: "60%",
-    },
+  bottom: {
+    backgroundColor: "#FFF8D6",
+    height: "60%",
+  },
 
-    searchBarContainer: {
-        borderRadius: 15,
-        backgroundColor: "#F7E1AE",
-        display: "flex",
-        flexDirection: "row",
-        marginRight: 80,
-        marginLeft: 35,
-        height: 30,
-    },
-})
+  searchBarContainer: {
+    borderRadius: 15,
+    backgroundColor: "#F7E1AE",
+    display: "flex",
+    flexDirection: "row",
+    //marginRight: 80,
+    marginLeft: 35,
+    width: 250,
+    height: 30,
+  },
+});
